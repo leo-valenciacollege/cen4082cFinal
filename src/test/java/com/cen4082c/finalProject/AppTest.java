7adfaa86197216aaca38dfbef2d10d1c06dfb749
Publio Samayoa <psamayoa@mail.valenciacollege.edu>
2024-04-21 14:34:04 -0400

    Adding Junit Test to test Fibconacci method

--- src/test/java/com/cen4082c/finalProject/AppTest.java
+++ src/test/java/com/cen4082c/finalProject/AppTest.java
@@ -5,34 +5,35 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
- * Unit test for simple App.
+ * Unit test for the App class.
  */
-public class AppTest 
-    extends TestCase
-{
+public class AppTest extends TestCase {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
-    {
-        super( testName );
+    public AppTest(String testName) {
+        super(testName);
     }
 
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    public static Test suite() {
+        return new TestSuite(AppTest.class);
     }
 
     /**
-     * Rigourous Test :-)
+     * Test the Fibonacci sequence calculation
      */
-    public void testApp()
-    {
-        assertTrue( true );
+    public void testCalculateFibonacci() {
+        assertEquals(0, App.calculateFibonacci(0));
+        assertEquals(1, App.calculateFibonacci(1));
+        assertEquals(1, App.calculateFibonacci(2));
+        assertEquals(2, App.calculateFibonacci(3));
+        assertEquals(3, App.calculateFibonacci(4));
+        assertEquals(5, App.calculateFibonacci(5));
+        assertEquals(8, App.calculateFibonacci(6));
     }
 }
