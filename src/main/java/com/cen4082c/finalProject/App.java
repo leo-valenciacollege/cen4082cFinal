0ed10ebab638aad6f10644647978148c26461fd8
Publio Samayoa <psamayoa@mail.valenciacollege.edu>
2024-04-21 14:06:41 -0400

    Updating App Class to use the Fibonacci sequence

--- src/main/java/com/cen4082c/finalProject/App.java
+++ src/main/java/com/cen4082c/finalProject/App.java
@@ -1,13 +1,32 @@
 package com.cen4082c.finalProject;
 
 /**
- * Hello world!
- *
+ * Final: Software Development Workflow
+ * The Main class contains methods to calculate and display Fibonacci numbers.
+ * Original Source: https://github.com/leo-valenciacollege/cen4082cFinal
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+public class App {
+	
+	/**
+     * Calculates the nth term in the Fibonacci sequence.
+     *
+     * @param n The position of the term to calculate.
+     * @return The nth term in the Fibonacci sequence.
+     */
+	
+	public static int calculateFibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return calculateFibonacci(n - 1) + calculateFibonacci(n - 2);
+        }
     }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 10;
+        int result = calculateFibonacci(n);
+        System.out.println("The " + n + "th term of the Fibonacci sequence is " + result + ".");
+	}
 }
